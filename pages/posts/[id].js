588d070fab34c3ed38dcaa3bd81aca16739d8117
Marc Bakker <webdev@marcbakker.com>
2021-04-20 18:44:53 +0200

    Add
    ...getStaticProps. But still not working...

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,26 @@
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Layout from "../../components/layout";
 
-const Post = (params) => {};
+const Post = ({ data }) => {
+    return (
+        <Layout>
+            <div>{data.title}</div>
+            <div>{data.id}</div>
+            <div>{data.date}</div>
+        </Layout>
+    );
+};
 
 const getStaticPaths = async () => {
     const paths = getAllPostIds();
-
     return { paths, fallback: false };
 };
 
-const getStaticProps = async () => {};
+const getStaticProps = async ({ params }) => {
+    const data = getPostData(params.id);
+
+    return { props: { data } };
+};
 
 export default Post;
 export { getStaticPaths, getStaticProps };
