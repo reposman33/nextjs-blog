60de752cf4d98ea2c355879fbbdc32cbd56d2c02
Marc Bakker <webdev@marcbakker.com>
2021-04-26 18:44:39 +0200

    Add a function _getAllPostIds to retrieve all files  in a Catch-all Routes format ([])

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import { getAllPostIds, getPostData } from "../../lib/posts";
+import { getAllPostIds, getPostData, _getAllPostIds } from "../../lib/posts";
 import Date from "../../components/date";
 import Head from "next/head";
 import utilStyles from "../../styles/utils.module.css";
@@ -23,6 +23,20 @@ const Post = ({ data }) => {
 
 const getStaticPaths = async () => {
     const paths = getAllPostIds();
+
+    // Implementation of Catch-all Routes (https://nextjs.org/learn/basics/dynamic-routes/dynamic-routes-details)
+    // To retrieve not only the files in the post directory but also the files of it's subdirectories call
+    // _getAllPostIds. This function returns the full path of each file in and below /posts.
+    // We have to process the strings here. Doing so in /lib/posts results in error messages
+    // since some paths are []...
+    const ids = _getAllPostIds().map((path) => {
+        // convrt the fullPath string to an array
+        const pathArray = path.split("\\");
+        // only keep the entries after the 'posts' directory
+        return pathArray.slice(pathArray.findIndex((p) => p === "posts") + 1);
+    });
+    console.log("ids = ", ids);
+
     return { paths, fallback: false };
 };
 
